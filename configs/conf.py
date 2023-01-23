eac0c8c0eb2fab55e540e2649157fb1b74d8d6ad
Yucheng Zhang <zhan4429@brown-fe03.rcac.purdue.edu>
2023-01-23 10:32:42 -0500

    remove docset

--- configs/conf.py
+++ configs/conf.py
@@ -4,9 +4,6 @@ import sys
 import sphinx_rtd_theme
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static", f"{docset}/_static"]
-html_css_files = ["css/custom.css", "css/sphinx_prompt_css.css"]
-html_js_files = ["js/expand_tabs.js"]
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_logo = "configs/rcac.jpeg"
 html_theme_options = {
